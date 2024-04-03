278edde30732682df7109cc52f155fbceca6aaa0
riki-rya <ixora3110@gmail.com>
2024-04-04 00:09:50 +0900

    delete '

--- src/app/about/aboutcomp/hero.tsx
+++ src/app/about/aboutcomp/hero.tsx
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <div>
     <header className="hero__header">
-      <h1 className="text-color">rrr's Portfolio site</h1>
+      <h1 className="text-color">rrrs Portfolio site</h1>
       <h2 className="font-bold text-cyan-600 transition-all duration-1000 ease-out dark:text-cyan-300">NANNTYATTE DEVELOPER</h2>
     </header>
     </div>
