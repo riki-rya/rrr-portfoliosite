12698002aaa860420bb6c4655cd0ce8baf494cd6
riki-rya <ixora3110@gmail.com>
2025-07-27 23:11:07 +0900

    fix

--- src/app/components/Contactlist.tsx
+++ src/app/components/Contactlist.tsx
@@ -10,12 +10,12 @@ const Contactlist = () => {
     <div className="flex flex-col items-center m-3 gap-3">
       <h1 className="text-2xl font-bold mb-8 text-center">Contact List</h1>
         <Button asChild className="w-40">
-            <a href="https://twitter.com/ryaaryu" target="_blank" rel="noopener noreferrer">
+            <a href="https://twitter.com/_ixoray" target="_blank" rel="noopener noreferrer">
             <FaSquareXTwitter className="mr-2 h-4 w-4" />Twitter（X）
             </a>
         </Button>
         <Button asChild className="w-40">
-            <a href="https://discord.com/users/147617894249922560" target="_blank" rel="noopener noreferrer">
+            <a href="https://discord.com/users/476358303694192640" target="_blank" rel="noopener noreferrer">
             <FaDiscord className="mr-2 h-4 w-4" />Discord
             </a>
         </Button>
